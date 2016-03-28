774104c245beb2ba1c04f473d9a14c26e7baca42
zheng-ji <407562752@qq.com>
2016-03-28 23:21:31 +0800

    add benchmark

--- kv_test.go
+++ kv_test.go
@@ -3,6 +3,7 @@ package bcastkv
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"testing"
 )
 
@@ -22,4 +23,42 @@ func TestPutAndGet(t *testing.T) {
 	var answer string
 	kv.Get(key, &answer)
 	fmt.Println(answer)
+}
+
+func Benchmark_Put(b *testing.B) {
+	b.StopTimer()
+	kv, err := NewBcastKv("test.kv")
+	if err != nil {
+		log.Fatal("Can not open database file")
+	}
+	defer kv.Close()
+
+	b.StartTimer() //重新开始时间
+	for i := 0; i < b.N; i++ {
+		key := "key_" + strconv.Itoa(i)
+		val := "val_" + strconv.Itoa(i)
+		err = kv.Put(key, val)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func Benchmark_Get(b *testing.B) {
+	b.StopTimer()
+	kv, err := NewBcastKv("test.kv")
+	if err != nil {
+		log.Fatal("Can not open database file")
+	}
+	defer kv.Close()
+
+	b.StartTimer() //重新开始时间
+	var answer string
+	for i := 0; i < b.N; i++ {
+		key := "key_" + strconv.Itoa(i)
+		err = kv.Get(key, &answer)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
